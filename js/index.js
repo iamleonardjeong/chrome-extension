2c52b5ef16b5d16f223df524fa218f2fe598f2e8
iamleonardjeong <iamleonardjeong@gmail.com>
2021-03-12 20:42:48 +0900

    add function: bg color change, components clear
    1. 배경 컬러 변경 기능 추가
    2. 컴포넌트 지우기 기능 추가

--- js/index.js
+++ js/index.js
@@ -1,36 +1,79 @@
-let body = document.querySelector('body');
 let container = document.getElementById('container');
 
-// text color change - form element add
+// browser right - bottom elements
+// text color, bg color, clear
 let textColor = document.getElementById('text-color');
-textColor.addEventListener('click', () => {
-  let formEl = document.createElement('div');
-  formEl.innerHTML = `
-  <form id="change-color">
-    <strong>Text color</strong>
-    <input id="color" type="text" placeholder="body color" />
-    <div id="change-color-buttons">
-      <input type="submit" value="Change" />
-      <button id="change-color-cancel">Cancel</button>
-    <div>
-  </form>
-  `;
-  body.appendChild(formEl);
+let bgColor = document.getElementById('bg-color');
+let clear = document.getElementById('clear');
 
-  // change color event
-  let changeTarget = document.getElementById('change-color');
-  changeTarget.addEventListener('submit', (event) => {
-    let color = document.getElementById('color');
-    container.style.color = color.value;
-
-    event.preventDefault();
-  });
-
-  // remove form event
-  let cancelBtn = document.getElementById('change-color-cancel');
-  cancelBtn.addEventListener('click', () => {
-    let removeTarget = document.getElementById('change-color');
-    console.log(removeTarget);
-    removeTarget.remove();
-  });
+// events
+textColor.addEventListener('click', changeColor(textColor));
+bgColor.addEventListener('click', changeColor(bgColor));
+clear.addEventListener('click', () => {
+  while (container.firstChild) {
+    container.firstChild.remove();
+  }
 });
+
+// function: change color
+function changeColor(place) {
+  place.addEventListener('click', () => {
+    if (document.getElementsByClassName('change-box').length < 1) {
+      let formEl = document.createElement('div');
+      formEl.className = 'change-color-form';
+      formEl.innerHTML = `
+        <form id="change-color">
+          <strong>${place.innerText} Change</strong>
+          <input id="color" type="text" placeholder="Color Name" />
+          <div id="change-color-buttons">
+            <button type="submit">Change</button>
+            <button id="change-color-cancel" type="reset">Cancel</button>
+          <div>
+        </form>
+      `;
+
+      document.body.appendChild(formEl);
+
+      let focusEl = document.getElementById('color');
+      focusEl.focus();
+
+      // change color event
+      let changeTarget = document.getElementById('change-color');
+      changeTarget.addEventListener('submit', (event) => {
+        let color = document.getElementById('color');
+        console.log(color.value);
+
+        if (event.target.childNodes[1].innerText === 'Text Color Change') {
+          container.style.color = color.value;
+        } else if (event.target.childNodes[1].innerText === 'Bg Color Change') {
+          container.style.backgroundColor = color.value;
+        }
+
+        event.preventDefault();
+      });
+
+      // remove form event
+      let cancelBtn = document.getElementById('change-color-cancel');
+      cancelBtn.addEventListener('click', () => {
+        let removeTarget = document.getElementsByClassName('change-color-form');
+        removeTarget[0].remove();
+      });
+    }
+  });
+}
+
+// let h1 = document.querySelector('h1');
+// h1.addEventListener('mouseover', () => {
+//   h1.childNodes[1].style.display = 'inline';
+// });
+// h1.addEventListener('mouseleave', () => {
+//   h1.childNodes[1].style.display = 'none';
+// });
+
+// let ok = document.querySelector('#ok');
+// ok.addEventListener('mouseover', () => {
+//   ok.childNodes[1].style.display = 'inline';
+// });
+// ok.addEventListener('mouseleave', () => {
+//   ok.childNodes[1].style.display = 'none';
+// });
